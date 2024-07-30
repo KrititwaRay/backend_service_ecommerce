19a18d918a3ab1feab588eacd18a3683f63f5798
krititwa Ray <krititwaray97@gmail.com>
2024-07-30 10:43:11 +0530

    set app routing

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const PORT = 3000;
 
 
 
+
+import { app_routing } from './app_routing.js';
+
+app.use('v1',app_routing)
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port : ${PORT}`);
 })
