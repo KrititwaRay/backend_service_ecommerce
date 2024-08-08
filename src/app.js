5125d47d903912275750eb665a1e403218390344
krititwa Ray <krititwaray97@gmail.com>
2024-08-08 12:00:53 +0530

    connect with database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import express from 'express';
-const app = express();
-const PORT = 3000;
+import dotenv from 'dotenv';
+dotenv.config();
 
+import { Config } from './configuration/config.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+import morgan from 'morgan';
+
+
+global.connectionObj = new  Config().connectMongoDb();
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(express.json({ liimit: '15mb'}));
 
 
 
@@ -9,7 +22,7 @@ const PORT = 3000;
 
 import { app_routing } from './app_routing.js';
 
-app.use('v1',app_routing)
+app.use('/v1',app_routing)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port : ${PORT}`);
