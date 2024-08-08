6e421be24c03ba4ffdecde73dc3dcf3d82ad40dc
krititwa Ray <krititwaray97@gmail.com>
2024-08-08 12:40:35 +0530

    modify connection

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -3,17 +3,12 @@ import mongoose from "mongoose";
 export class Config {
     constructor() {
         this.dbname = 'krititwaDB';
-        this.dbPass = '';
         this.dbUrl = `mongodb://127.0.0.1:27017/${this.dbName}`
     }
 
 
-    async connectMongoDb() {
-        // mongoose.connect(this.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-        //     console.log('Database connection has been established successfully.');
-
-        // }).catch(err => console.log(console.error('Unable to connect to the database:', err)))
-        await mongoose.connect(this.dbUrl).then(() => {
+    connectMongoDb() {
+        mongoose.connect(this.dbUrl).then(() => {
             console.log('Database connection has been established successfully.');
 
         }).catch(err => console.log(console.error('Unable to connect to the database:', err)))
